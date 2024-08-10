53494327a7ff58dda000f9cda4a43c633ecbe08b
Your Name <your.email@example.com>
2024-08-10 23:33:18 +0900

    値受け渡しテスト

--- python/book/views.py
+++ python/book/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  return render(request, 'index.html')
+
+def baidu(request):
+  return render(request, 'baidu.html')
+
+def info(request):
+  username = '承太郎'
+  book = {'manga': '鬼滅の刃', 'syousetu': '人間失格'}
+  books = [
+    {'name': 'コイキング', 'type': '水'},
+    {'name': 'ギャラドス', 'type': '水・飛行'},
+  ]
+  context = {
+    'username': username,
+    'book': book,
+    'books': books
+  }
+  return render(request, 'info.html', context=context)
