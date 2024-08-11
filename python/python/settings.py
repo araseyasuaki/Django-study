1ec7539357c2b0cadc1eb52b4bf923a945270a66
Your Name <your.email@example.com>
2024-08-11 23:55:12 +0900

    画像url表示テスト

--- python/python/settings.py
+++ python/python/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins':['django.templatetags.static']
         },
     },
 ]
@@ -117,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- python/python/urls.py
+++ python/python/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from book import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +33,4 @@ urlpatterns = [
     path('filter', views.filter_view, name='filter'),
     path('template/from', views.template_form, name="template_form"),
     path('sta', views.static_demo, name='static_view')
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
